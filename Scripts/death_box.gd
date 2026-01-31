c1f2be1995909c344ff9fe26210fe6a2b906d847
Graham Baker <grahambakerhere@gmail.com>
2026-01-31 16:25:21 -0500

    fixed merge conflict
    okay we're good now i promise

--- Scripts/death_box.gd
+++ Scripts/death_box.gd
@@ -3,17 +3,9 @@ extends Area2D
 signal death
 
 
-<<<<<<< HEAD
 # this is just a proof of concept for Thing That Kills You - you can replace/delete
 # when we start Doing Things
 
 func _on_area_entered(_area: Area2D) -> void:
 	#print("death box entered")
 	death.emit() 
-=======
-
-
-func _on_area_entered(area: Area2D) -> void:
-	print("death box entered")
-	death.emit() # Replace with function body.
->>>>>>> 4ca1aa36e63c707fba929dfdc04ae0af7383b64b
