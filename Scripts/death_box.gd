bf485fc50cea8901cff79ca3e55d69e3a296d339
Graham Baker <grahambakerhere@gmail.com>
2026-01-31 16:14:43 -0500

    added animal sprites and third collider
    added player sprites and animations for each form and resized colliders to match. climbing restricted to monkey and ester. rabbit and monkey have their own collider, with their own raycasts to check if you can change forms to them when you couldn't change into ester.

---
+++ Scripts/death_box.gd
@@ -0,0 +1,11 @@
+extends Area2D
+
+signal death
+
+
+# this is just a proof of concept for Thing That Kills You - you can replace/delete
+# when we start Doing Things
+
+func _on_area_entered(_area: Area2D) -> void:
+	#print("death box entered")
+	death.emit() 
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -3,9 +3,18 @@ extends CharacterBody2D
 class_name Player
 
 @onready var rat_collider: CollisionShape2D = $RatCollider
-@onready var normal_collider: CollisionShape2D = $NormalCollider
-@onready var left_above_cast: RayCast2D = $LeftCast
-@onready var right_above_cast: RayCast2D = $RightCast
+@onready var normal_collider: CollisionShape2D = $EsterCollider
+@onready var animal_collider: CollisionShape2D = $AnimalCollider
+@onready var left_ester_cast: RayCast2D = $LeftEsterCast
+@onready var right_ester_cast: RayCast2D = $RightEsterCast
+@onready var left_animal_cast: RayCast2D = $LeftAnimalCast
+@onready var right_animal_cast: RayCast2D = $RightAnimalCast
+
+@onready var ester_animator: AnimatedSprite2D = $Animations/EsterAnimations
+@onready var rabbit_animator: AnimatedSprite2D = $Animations/RabbitAnimations
+@onready var monkey_animator: AnimatedSprite2D = $Animations/MonkeyAnimations
+@onready var rat_animator: AnimatedSprite2D = $Animations/RatAnimations
+
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
@@ -24,37 +33,65 @@ var can_climb: bool = false
 # whether you're in a ladder
 var lantern_bounces: int = 0
 # number of times you've bounced on a lantern since leaving the ground
-var spawnpoint: Vector2 = Vector2(934.0, 553.0)
+var spawnpoint: Vector2 = Vector2(0, 0)
+# where you return to after you die
+var flipped: bool = false
+# whether you are facing left (the sprite is flipped) or not
+var current_animator: AnimatedSprite2D
 
 func _ready() -> void:
-	print(spawnpoint)
+	spawnpoint = global_position
+	current_animator = ester_animator
 	pass
 
 func _physics_process(delta: float) -> void:
+	current_animator.flip_h = flipped
+	
+	ester_animator.visible = current_animator == ester_animator
+	rat_animator.visible = current_animator == rat_animator
+	rabbit_animator.visible = current_animator == rabbit_animator
+	monkey_animator.visible = current_animator == monkey_animator
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		current_animator.play("Jump")
 	else:
 		jump_count = 0
 		lantern_bounces = 0
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump"):
-		if jump_count < max_jump_count:
-			jump()
-			jump_count += 1
+	
 	
 	# Climbing ladders
-	if Input.is_action_pressed("jump") and can_climb:
-		velocity.y = CLIMB_VELOCITY
+	if can_climb:
+		if Input.is_action_pressed("jump") and (mask_index == 0 or mask_index == 2):
+			velocity.y = CLIMB_VELOCITY
+			if not is_on_floor():
+				current_animator.play("Climb")
+		
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
+		if direction > 0:
+			flipped = false
+		if direction < 0:
+			flipped = true
+		if is_on_floor():
+			current_animator.play("Walk")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		if is_on_floor():
+			current_animator.play("Idle")
+	
+	if Input.is_action_just_pressed("jump"):
+			if jump_count < max_jump_count:
+				jump()
+				jump_count += 1
+
  # none, rabbit, monkey, rat
 	if Input.is_action_just_pressed("mask_next"):
 		mask_change(mask_index + 1 if mask_index + 1 < MASK_LIST.size() else 0)
@@ -66,24 +103,38 @@ func _physics_process(delta: float) -> void:
 	
 func jump(power = JUMP_VELOCITY) -> void:
 		velocity.y = power
+		current_animator.play("Jump")
 
 func mask_change(new_mask: int) -> void:
-	if left_above_cast.is_colliding() or right_above_cast.is_colliding():
+	if (left_ester_cast.is_colliding() or right_ester_cast.is_colliding()) and new_mask == 0:
 		return
 	else:
 		mask_index = new_mask
 		var mask: String = MASK_LIST[mask_index]
 		
-		# rabbit:
+		# jumps:
 		max_jump_count = 2 if mask == "rabbit" else 1
 		
-		# rat:
-		normal_collider.disabled = true if mask == "rat" else false
-		rat_collider.disabled = not normal_collider.disabled
+		# colliders:
+		normal_collider.disabled = true if mask != "none" else false
+		rat_collider.disabled = true if mask != "rat" else false
+		animal_collider.disabled = true if mask != "rabbit" and mask != "monkey" else false
 		
-		# monkey:
+		# monkey collision mask:
 		set_collision_mask_value(3, (mask=="monkey"))
 		print(get_collision_mask_value(3))
+		
+		match mask: # is there a way to do this as part of the variable assignment?
+			"none":
+				current_animator = ester_animator
+			"rabbit":
+				current_animator = rabbit_animator
+			"monkey":
+				current_animator = monkey_animator
+			"rat":
+				current_animator = rat_animator
+			_:
+				current_animator = ester_animator
 		
 func _on_bounce_area_body_entered(_body: Node2D) -> void:
 	if mask_index == 2:
