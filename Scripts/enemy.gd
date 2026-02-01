2b1087540a9d963f49c98426649a6ae6036a97ad
Graham Baker <grahambakerhere@gmail.com>
2026-02-01 01:15:30 -0500

    updated enemy suspicion
    is this how it's supposed to work? if not i can def fix it

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -5,7 +5,6 @@ extends CharacterBody2D
 @onready var lower_ray: RayCast2D = $LowerRay
 @onready var enemy_sprite: Sprite2D = $EnemySprite
 @onready var watching_eye: Sprite2D = $WatchedSymbol
-@onready var ester_collider: CollisionShape2D = $/root/GameMaster/Player/EsterCollider
 
 const SPEED = 200.0
 const TIME_TO_AGGRO: float = 3
@@ -28,6 +27,7 @@ var enemy_suspicion: String = "normal" # normal, suspicion, aggro
 # aggro - enemy chases limited distance after player before returning to suspicion, then normal
 
 var player_changed: bool = false
+# true if the player changed masks this frame. used for determining aggro.
 
 func _ready() -> void:
 	origin = global_position
@@ -48,18 +48,22 @@ func _physics_process(delta: float) -> void:
 	if upper_ray.is_colliding():
 			if upper_ray.get_collider().is_class("CharacterBody2D"):
 				scanned_player = upper_ray.get_collider()
-			elif lower_ray.get_collider().is_class("CharacterBody2D"):
+	elif lower_ray.is_colliding():
+			if lower_ray.get_collider().is_class("CharacterBody2D"):
 				scanned_player = lower_ray.get_collider()
-			elif middle_ray.get_collider().is_class("CharacterBody2D"):
+	elif middle_ray.is_colliding():
+			if middle_ray.get_collider().is_class("CharacterBody2D"):
 				scanned_player = middle_ray.get_collider()
 	if scanned_player:
 		if scanned_player.mask_index == 0:
 			enemy_suspicion = "aggro"
 			peace_timer = TIME_TO_COOLDOWN
 		if scanned_player.mask_index == 2:
-			enemy_suspicion = "suspicious"
+			if enemy_suspicion != "aggro": enemy_suspicion = "suspicious"
+			print("a monkey!")
 		if player_changed:
 			enemy_suspicion = "aggro"
+	else: aggression_timer = TIME_TO_AGGRO
 		
 	if enemy_suspicion == "normal":
 		if origin.x < destination.x:
@@ -73,19 +77,28 @@ func _physics_process(delta: float) -> void:
 				facing_right = true
 			if global_position.x <= origin.x:
 				facing_right = false
+		watching_eye.modulate = Color.WHITE
 				
 	if enemy_suspicion == "aggro":
-		print("i'm angry!!!!!!!")
-		enemy_suspicion = "normal"
+		peace_timer -= delta
+		watching_eye.modulate = Color.WHITE.lerp(Color.RED, peace_timer / TIME_TO_COOLDOWN)
+		if peace_timer <= 0: enemy_suspicion = "normal"
+		
+		
+	if enemy_suspicion == "suspicious":
+		aggression_timer -= delta
+		print(aggression_timer)
+		watching_eye.modulate = Color.YELLOW.lerp(Color.WHITE, aggression_timer / TIME_TO_AGGRO)
+		if aggression_timer <= 0: 
+			enemy_suspicion = "aggro"
 	
 	# if enemy is outside the two points set for them to walk, they flip
 	# sorry there's probably a more eloquent way to do this. if it doesn't work
 	# or you want it done differently i'll change it
 	
 	velocity.x = SPEED * (1 if facing_right else -1)
+	player_changed = false
 	move_and_slide()
-	
-
 
 
 func _on_player_player_mask_change(_new_mask: int) -> void:
